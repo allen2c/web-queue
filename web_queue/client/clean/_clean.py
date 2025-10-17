147940a28c8854acc39defcc2b16ad85c0c22abb
AllenChou <f1470891079@gmail.com>
2025-10-17 19:20:23 +0800

    feat(client): ✨ Add HTML cleaning and logging dependencies
    Integrate new dependencies for enhanced logging (logfire, logging_bullet_train, OpenTelemetry packages, and protobuf) and introduce a Clean class in web_queue/client/clean/ for processing and cleaning HTML content using BeautifulSoup and HTMLCleaner utilities.

---
+++ web_queue/client/clean/_clean.py
@@ -0,0 +1,21 @@
+import typing
+
+import bs4
+
+from web_queue.client import WebQueueClient
+from web_queue.utils.html_cleaner import HTMLCleaner
+
+
+class Clean:
+    def __init__(self, client: WebQueueClient):
+        self.client = client
+
+    def clean(self, html: bs4.BeautifulSoup | str) -> bs4.BeautifulSoup:
+        html = (
+            bs4.BeautifulSoup(html, "html.parser")
+            if isinstance(html, typing.Text)
+            else html
+        )
+
+        cleaned_html = HTMLCleaner.clean_as_main_content_html_str(html)
+        return bs4.BeautifulSoup(cleaned_html, "html.parser")
